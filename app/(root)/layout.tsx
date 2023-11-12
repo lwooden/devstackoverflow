f23149e51f2ec6bfb4ec927e0a42e17e29e6588d
lwooden <lowell.wooden@gmail.com>
2023-11-12 07:08:00 -0500

    bring in RightSidebar compo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/shared/leftsidebar/LeftSidebar"
 import Navbar from "@/components/shared/navbar/Navbar"
+import RightSidebar from "@/components/shared/rightsidebar/RightSidebar"
 import React from "react"
 
 // This Layout file defines the layout for every component under root
@@ -13,7 +14,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSidebar
+        <RightSidebar />
       </div>
       Toaster
     </main>
