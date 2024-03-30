d3236f5396dfdcebe0fe4318c06e874042670334
lwooden <lowell.wooden@gmail.com>
2024-03-30 17:15:56 -0400

    database connectivity

---
+++ lib/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+let isConnected: boolean = false
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true)
+
+  if (!process.env.MONGODB_URL) {
+    console.log("=> MongoDB URL is not defined!")
+  }
+
+  if (isConnected) {
+    console.log("=> using existing database connection")
+    return
+  }
+
+  console.log("=> using new database connection")
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "devstackoverflow",
+    })
+
+    isConnected = true
+    console.log("=> database connected")
+  } catch (error) {
+    console.log("=> error connecting to database:", error)
+  }
+}
