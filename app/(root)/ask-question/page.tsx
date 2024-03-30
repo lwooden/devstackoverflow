24704238e0b334756e5d4b93c3d586454f75dc8c
lwooden <lowell.wooden@gmail.com>
2024-03-30 17:13:33 -0400

    mock user + get user server action

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,22 @@
+import Question from "@/components/forms/Question"
+import { getUserById } from "@/lib/actions/user.actions"
+import { redirect } from "next/navigation"
 import React from "react"
 
-const page = () => {
-  return <div>Ask Question</div>
+const Page = async () => {
+  const userId = "123456"
+  if (!userId) redirect("/sign-in")
+
+  const mongoUserId = await getUserById({ userId })
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div>
+        <Question mongoUserId={JSON.stringify(mongoUserId._id)} />
+      </div>
+    </div>
+  )
 }
 
-export default page
+export default Page
