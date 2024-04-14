1c762b93b12e995674b9f402de991e1b21afbe81
lwooden <lowell.wooden@gmail.com>
2024-04-14 18:42:50 -0400

    resolve eslint warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 
 import { ClerkProvider } from "@clerk/nextjs"
 
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google"
 import type { Metadata } from "next"
 
@@ -33,6 +34,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
+  // eslint-disable-next-line no-undef
   children: React.ReactNode
 }) {
   return (
