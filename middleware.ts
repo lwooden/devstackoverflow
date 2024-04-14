d0182a581521ec5f42939a58d446f55144dda7c2
lwooden <lowell.wooden@gmail.com>
2024-04-14 19:58:16 -0400

    webhook integration

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,19 @@ import { authMiddleware } from "@clerk/nextjs"
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  // public routes that don't require authentication
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  // routes to ignore middleware processing
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 })
 
 export const config = {
