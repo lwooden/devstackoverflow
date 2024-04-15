6808a44e914a710b553389ff61da80f0fa0cd91e
lwooden <lowell.wooden@gmail.com>
2024-04-14 22:25:33 -0400

    update imgUrl to grab url from clerk

--- components/QuestionCard.tsx
+++ components/QuestionCard.tsx
@@ -85,7 +85,7 @@ const QuestionCard = ({
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         {/* Re-uesable Version */}
         <Metric
-          imgUrl="/assets/icons/hamburger.svg"
+          imgUrl={author.picture}
           alt="user"
           value={author.username}
           title={` - asked ${getTimestamp(createdAt)}`}
