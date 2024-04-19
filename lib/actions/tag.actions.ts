9d5f153fe133ec3788affd3b8ca75cd634497c39
lwooden <lowell.wooden@gmail.com>
2024-04-19 15:50:13 -0400

    tag action update

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,15 +1,28 @@
 "use server"
 
-import User from "@/database/user.model"
+import Tag from "@/database/tag.model"
 import { connectToDatabase } from "../mongoose"
-import { GetTopInteractedTagsParams } from "./shared.types"
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types"
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase()
+
+    const tags = await Tag.find({})
+
+    return { tags }
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase()
 
-    const { userId } = params
-    const user = await User.findById(userId)
+    // const { userId } = params
+    // const user = await User.findById(userId)
 
     return [
       { _id: "1", name: "tag1" },
