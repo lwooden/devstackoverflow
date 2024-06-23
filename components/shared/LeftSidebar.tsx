e92af1125de65b4eeeaa76c8a7d14470c87e7f0b
lwooden <lowell.wooden@gmail.com>
2024-06-23 18:27:42 -0400

    route to profile/id when clicking profile

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,9 +5,10 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import React from "react"
-import { SignedOut } from "@clerk/nextjs"
+import { SignedOut, useAuth } from "@clerk/nextjs"
 
 const LeftSidebar = () => {
+  const { userId } = useAuth()
   const pathname = usePathname()
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between  overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
@@ -17,6 +18,13 @@ const LeftSidebar = () => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route
+
+          if (link.route === "/profile") {
+            if (userId) {
+              link.route = `${link.route}/${userId}`
+            }
+          }
+
           return (
             <Link
               key={link.route}
