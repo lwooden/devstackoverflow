0fc53aa20c3a0dcffebcd0e7c0bb250d36f49399
lwooden <lowell.wooden@gmail.com>
2024-06-23 16:28:48 -0400

    Votes component update

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,164 @@
-import React from "react"
+"use client"
 
-const Votes = () => {
-  return <div>Votes</div>
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.actions"
+import { viewQuestion } from "@/lib/actions/interaction.actions"
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.actions"
+import { toggleSaveQuestion } from "@/lib/actions/user.actions"
+import { formatAndDivideNumber } from "@/lib/utils"
+import Image from "next/image"
+import { usePathname, useRouter } from "next/navigation"
+import React, { useEffect } from "react"
+
+interface Props {
+  type: string
+  itemId: string
+  userId: string
+  upvotes: number
+  hasUpVoted: boolean
+  downvotes: number
+  hasDownVoted: boolean
+  hasSaved?: boolean
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasUpVoted,
+  downvotes,
+  hasDownVoted,
+  hasSaved,
+}: Props) => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const handleSave = async () => {
+    await toggleSaveQuestion({
+      questionId: JSON.parse(itemId),
+      userId: JSON.parse(userId),
+      path: pathname,
+    })
+  }
+
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      return
+    }
+
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        })
+      } else if (type === "Answer") {
+        await upvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        })
+      }
+    }
+    // TODO: show a toast
+
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        })
+      } else if (type === "Answer") {
+        await downvoteAnswer({
+          answerId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted: hasUpVoted,
+          hasdownVoted: hasDownVoted,
+          path: pathname,
+        })
+      }
+    }
+
+    // TODO: show a toast
+  }
+
+  // Every time this componet is rendered, run the viewQuestion server action
+  useEffect(() => {
+    viewQuestion({
+      questionId: JSON.parse(itemId),
+      userId: userId ? JSON.parse(userId) : undefined,
+    })
+  }, [itemId, userId, pathname, router])
+
+  return (
+    <div className="flex gap-5">
+      {/* Wrap everything in a higher level div */}
+      <div className="flex-center gap-2.5">
+        <div className=" flex-center gap-1.5">
+          <Image
+            src={
+              hasUpVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer"
+            onClick={() => handleVote("upvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+          <Image
+            src={
+              hasDownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="downvote"
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* it is best to utilize the logical AND operator here for conditional rendering */}
+      {type === "Question" && (
+        <Image
+          src={
+            hasSaved
+              ? "/assets/icons/star-filled.svg"
+              : "/assets/icons/star-red.svg"
+          }
+          width={18}
+          height={18}
+          alt="star"
+          className="cursor-pointer"
+          onClick={handleSave}
+        />
+      )}
+    </div>
+  )
 }
 
 export default Votes
